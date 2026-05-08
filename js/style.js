5591615c86656823fa8af9c7bd53c03149d37096
Felipe Santana <fsantanalopes3@gmail.com>
2026-05-08 20:35:07 -0300

    lazy

--- js/style.js
+++ js/style.js
@@ -66,3 +66,17 @@ if (linkProdutos) {
 });
 
 
+const imagens = document.querySelectorAll('.lazy');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      img.classList.add('loaded');
+      observer.unobserve(img);
+    }
+  });
+});
+
+imagens.forEach(img => observer.observe(img));
